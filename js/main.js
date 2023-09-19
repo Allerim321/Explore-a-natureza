a7618ab3d619a646823147769016fe8f09b85687
Mirella <mirellatcg@gmail.com>
2023-09-19 14:39:07 -0300

    up

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-console.log("olá mundo!!!")
+const imagens = document.querySelectorAll('.js-conteudo li')
+const textos = document.querySelectorAll('.js-conteudo section')
+
+console.log(imagens)
+console.log(textos)
+
+function ativarConteudo(indice){
+    textos.forEach((item) =>{
+        item.classList.remove('active')
+    })
+    textos[indice].classList.add('active')
+}
+
+textos.forEach((item, indice) =>{
+    item.addEventListener('click',() =>{
+    ativarConteudo(indice)
+    })
+})
